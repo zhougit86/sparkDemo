de9c4706c57aa6ad109d90b8347fe7c8fc933e60
zhouxiaohu <80839961@yonghui.com>
2019-06-28 20:04:20 +0800

    scala learn

--- src/main/scala/main/helloWorld/first.scala
+++ src/main/scala/main/helloWorld/first.scala
@@ -10,20 +10,30 @@ object first {
     println("haha")
 
     //class的定义方法
-    var aa: DemoClass = new DemoClass()
-    aa.shout()
+//    var aa: DemoClass = new DemoClass()
+//    aa.shout()
+//
+//    val square2 = (x:Int)=>{println(x*2)}
+//
+//    //函数式编程的方法
+//    val numList = List(1, 2, 3, 4, 5, 6)
+////    for(a<-numList){
+////      println(a)
+////    }
+//    //函数方法1
+//    numList.foreach(square)
+//    //函数方法2
+//    numList.foreach(square2)
 
-    val square2 = (x:Int)=>{println(x*2)}
 
-    //函数式编程的方法
-    val numList = List(1, 2, 3, 4, 5, 6)
-//    for(a<-numList){
-//      println(a)
-//    }
-    //函数方法1
-    numList.foreach(square)
-    //函数方法2
-    numList.foreach(square2)
+    val answer = 2;
+    var answer1 = 2;
+
+    answer1 = answer1 +1 ;
+//    System.err.println(answer)
+//    System.err.println(answer1)
+//
+    1.to(10).foreach(u=>println(u))
 
 
   }
